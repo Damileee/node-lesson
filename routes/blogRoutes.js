f0ed0426c3d1011b8f71e252f4368e5c4794cf10
Damileee <olasojudamilare6@gmail.com>
2025-07-28 15:10:39 +0100

    feat: modularize blog routes by delegating to blogController for improved organization

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,57 +1,12 @@
 const express = require('express');
+const blogController = require('../controllers/blogController');
+
 const router = express.Router();
-const Blog = require('../models/blog');
 
-
-// blog routes
-// get all blogs from the database and render them
-router.get('/', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.post('/', (req, res) => {
-  const blog = new Blog(req.body);
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(404).render('404', { title: 'Blog not found' });
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
-// This file defines the routes for handling blog-related requests, including creating, reading, and deleting blogs. It uses Express Router to modularize the routes and interact with the Blog model for database operations.
